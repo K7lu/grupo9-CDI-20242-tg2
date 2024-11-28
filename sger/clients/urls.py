493e8e73519fc13e19d577c545558cd55fdb3976
Thassio <thxssio@gmail.com>
2024-11-28 14:18:08 -0300

    fix: Client

--- sger/clients/urls.py
+++ sger/clients/urls.py
@@ -1,9 +1,7 @@
+# clients/urls.py
 from django.urls import path
 from . import views
 
-app_name = 'clients'
-
 urlpatterns = [
-    path('', views.client_list, name='client_list'),
-    path('create/', views.create_client_view, name='create_client'),
+    path('', views.home, name='home'),  # Página inicial da app 'clients'
 ]
