a13cc139aaf8145715c796bd1349c2b157db6a3d
pedropotter01 <131699887+pedropotter01@users.noreply.github.com>
2024-12-02 11:42:06 -0300

    Update views.py

--- sger/employees/views.py
+++ sger/employees/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.db import IntegrityError
+from .models import Funcionario 
 
 def employee_registration(request):
-    # Lógica para exibir o formulário de cadastro de funcionários
+    if request.method == "POST":
+        nome = request.POST.get('nome')
+        cpf = request.POST.get('cpf')
+        data_contratacao = request.POST.get('data_contratacao')
+        telefone = request.POST.get('telefone')
+
+        try:
+            Funcionario.objects.create(
+                Nome=nome,
+                CPF=cpf,
+                Data_Contratacao=data_contratacao or None,  
+                Telefone=telefone
+            )
+            return redirect('success_page') 
+        except IntegrityError:
+            return render(request, 'employee_registration.html', {
+                'error_message': 'Erro: CPF já está registrado!'
+            })
+
     return render(request, 'employee_registration.html')
-def home(request):
-    return render(request, 'clients/home.html')
