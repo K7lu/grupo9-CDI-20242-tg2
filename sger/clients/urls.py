f1f16101e3208ef5b0facf25ede908a3aac13cec
K7lu <claudenirfilho02@gmail.com>
2024-12-05 16:12:58 -0300

    Clients templates and data manage added

--- sger/clients/urls.py
+++ sger/clients/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('', views.home, name='home'),  # Página inicial da app 'clients'
     path('clients_list/', views.clients_list, name='clients_list'),
     path('clients_register/', views.clients_register_view, name='clients_register'),
+    path('delete/<int:id>', views.delete_clients_view, name='delete_client'),
+    path('client/edit/<int:id>/', views.edit_client_view, name='edit_client'),
+    path('client/search/', views.clients_search_view, name='clients_search'),
 
 ]
